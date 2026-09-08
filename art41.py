b12878f1f997389623cf6598ce43fffa7c85d8b5
mdshahnawaz7095-ops <mdshahnawaz7095@gmail.com>
2026-09-08 20:16:10 +0530

    Add files via upload

---
+++ art41.py
@@ -0,0 +1,14 @@
+hour = int(input("Enter hour: "))
+minute = int(input("Enter minute: "))
+
+hour = hour % 12
+
+hour_angle = hour * 30 + minute * 0.5
+minute_angle = minute * 6
+
+angle = abs(hour_angle - minute_angle)
+
+if angle > 180:
+    angle = 360 - angle
+
+print("Smaller angle =", angle)
